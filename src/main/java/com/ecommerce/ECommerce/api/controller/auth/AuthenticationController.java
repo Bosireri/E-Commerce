ef29626f5c5689a39554429159621c46cedd2c1d
Bosireri <bosireri24@gmail.com>
2023-06-22 11:28:27 +0300

    Repo

--- src/main/java/com/ecommerce/ECommerce/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/ecommerce/ECommerce/api/controller/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.ECommerce.api.controller.auth;
 
 import com.ecommerce.ECommerce.api.model.RegistrationBody;
+import com.ecommerce.ECommerce.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+    private UserService userService;
+
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/register")
     public void registerUser(@RequestBody RegistrationBody registrationBody){
+        userService.registerUser(registrationBody);
     }
 }
--- src/main/java/com/ecommerce/ECommerce/service/UserService.java
+++ src/main/java/com/ecommerce/ECommerce/service/UserService.java
@@ -2,10 +2,18 @@ package com.ecommerce.ECommerce.service;
 
 import com.ecommerce.ECommerce.api.model.RegistrationBody;
 import com.ecommerce.ECommerce.model.LocalUser;
+import com.ecommerce.ECommerce.model.dao.LocalUserDAO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private LocalUserDAO localUserDAO;
+
+    public UserService(LocalUserDAO localUserDAO) {
+        this.localUserDAO = localUserDAO;
+    }
+
     public LocalUser registerUser(RegistrationBody registrationBody) {
         LocalUser user = new LocalUser();
         user.setEmail(registrationBody.getEmail());
@@ -14,6 +22,7 @@ public class UserService {
         user.setUsername(registrationBody.getUsername());
         //TODO: Encrypt passwords!!
         user.setPassword(registrationBody.getPassword());
-
+        user=localUserDAO.save(user);
+        return localUserDAO.save(user);
     }
 }
