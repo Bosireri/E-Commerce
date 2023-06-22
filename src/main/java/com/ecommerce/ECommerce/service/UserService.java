cf0771a910c2ca5ea92368e119961e268f496d0b
Bosireri <bosireri24@gmail.com>
2023-06-22 10:57:17 +0300

    User.service

---
+++ src/main/java/com/ecommerce/ECommerce/service/UserService.java
@@ -0,0 +1,18 @@
+package com.ecommerce.ECommerce.service;
+
+import com.ecommerce.ECommerce.api.model.RegistrationBody;
+import com.ecommerce.ECommerce.model.LocalUser;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public LocalUser registerUser(RegistrationBody registrationBody) {
+        LocalUser user = new LocalUser();
+        user.setEmail(registrationBody.getEmail());
+        user.setFirstName(registrationBody.getFirstName());
+        user.setLastName(registrationBody.getLastName());
+        user.setUsername(registrationBody.getUsername());
+        //TODO: Encrypt passwords!!
+        user.setPassword(registrationBody.getPassword());
+    }
+}
