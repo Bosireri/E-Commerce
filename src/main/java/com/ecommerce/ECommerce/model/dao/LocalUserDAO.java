ec47c7acaf2c0eff7a49b1fddbf14cc2411050e9
Bosireri <bosireri24@gmail.com>
2023-06-22 15:07:45 +0300

    theRepo

--- src/main/java/com/ecommerce/ECommerce/model/dao/LocalUserDAO.java
+++ src/main/java/com/ecommerce/ECommerce/model/dao/LocalUserDAO.java
@@ -3,5 +3,11 @@ package com.ecommerce.ECommerce.model.dao;
 import com.ecommerce.ECommerce.model.LocalUser;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface LocalUserDAO extends CrudRepository<LocalUser, Long> {
+
+    Optional<LocalUser> findByUsernameIgnoreCase(String username);
+
+    Optional<LocalUser> findByEmailIgnoreCase(String email);
 }
