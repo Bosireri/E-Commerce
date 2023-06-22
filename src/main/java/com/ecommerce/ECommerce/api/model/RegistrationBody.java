31f69f7148d106580b01cb29c29b083e9f51841c
Bosireri <bosireri24@gmail.com>
2023-06-22 15:09:32 +0300

    signUp

--- src/main/java/com/ecommerce/ECommerce/api/model/RegistrationBody.java
+++ src/main/java/com/ecommerce/ECommerce/api/model/RegistrationBody.java
@@ -1,11 +1,27 @@
 package com.ecommerce.ECommerce.api.model;
 
+import jakarta.validation.constraints.*;
+
 public  class RegistrationBody {
 
+    @NotNull
+    @NotBlank
+    @Size (min=8, max=32)
     private String username;
+    @NotNull
+    @NotBlank
+    @Email
     private String email;
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
+    @Size (min=8, max=20)
     private String password;
+    @NotNull
+    @NotBlank
     private String firstName;
+    @NotNull
+    @NotBlank
     private String lastName;
 
     public String getUsername() {
