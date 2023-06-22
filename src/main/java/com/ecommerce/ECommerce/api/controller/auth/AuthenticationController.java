d18be579d017fd62d5a55305ccbf2882924a9a30
Bosireri <bosireri24@gmail.com>
2023-06-22 15:08:23 +0300

    theAuth

--- src/main/java/com/ecommerce/ECommerce/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/ecommerce/ECommerce/api/controller/auth/AuthenticationController.java
@@ -1,11 +1,13 @@
 package com.ecommerce.ECommerce.api.controller.auth;
 
 import com.ecommerce.ECommerce.api.model.RegistrationBody;
+import com.ecommerce.ECommerce.exception.UserAlreadyExistsException;
 import com.ecommerce.ECommerce.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,7 +20,12 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public void registerUser(@RequestBody RegistrationBody registrationBody){
-        userService.registerUser(registrationBody);
+    public ResponseEntity registerUser(@Valid @RequestBody RegistrationBody registrationBody){
+        try {
+            userService.registerUser(registrationBody);
+            return ResponseEntity.ok().build();
+        } catch (UserAlreadyExistsException ex) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
