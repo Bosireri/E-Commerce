4cd16b7e65e21dc28fe13821deb16be920e99b6e
Bosireri <bosireri24@gmail.com>
2023-06-22 10:59:27 +0300

    signUp

---
+++ src/main/java/com/ecommerce/ECommerce/api/model/RegistrationBody.java
@@ -0,0 +1,30 @@
+package com.ecommerce.ECommerce.api.model;
+
+public  class RegistrationBody {
+
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
