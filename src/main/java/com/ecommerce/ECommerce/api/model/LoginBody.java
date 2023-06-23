136e5d75516b352dec02c156b512fa49e814f2c7
Bosireri <bosireri24@gmail.com>
2023-06-23 15:52:03 +0300

    lBody

---
+++ src/main/java/com/ecommerce/ECommerce/api/model/LoginBody.java
@@ -0,0 +1,15 @@
+package com.ecommerce.ECommerce.api.model;
+
+public class LoginBody {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
