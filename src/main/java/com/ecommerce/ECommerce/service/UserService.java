9ed0a7aee7ece6ab35399de2375b60eb29e35d00
Bosireri <bosireri24@gmail.com>
2023-06-23 15:50:53 +0300

    service

--- src/main/java/com/ecommerce/ECommerce/service/UserService.java
+++ src/main/java/com/ecommerce/ECommerce/service/UserService.java
@@ -1,19 +1,26 @@
 package com.ecommerce.ECommerce.service;
 
+import com.ecommerce.ECommerce.api.model.LoginBody;
 import com.ecommerce.ECommerce.api.model.RegistrationBody;
 import com.ecommerce.ECommerce.exception.UserAlreadyExistsException;
 import com.ecommerce.ECommerce.model.LocalUser;
 import com.ecommerce.ECommerce.model.dao.LocalUserDAO;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     private LocalUserDAO localUserDAO;
+    private EncryptionService encryptionService;
+    private JWTService jwtService;
+
 //constructor
-    public UserService(LocalUserDAO localUserDAO) {
+    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService) {
         this.localUserDAO = localUserDAO;
+        this.encryptionService = encryptionService;
+        this.jwtService = jwtService;
     }
 
     public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
@@ -26,9 +33,19 @@ public class UserService {
         user.setFirstName(registrationBody.getFirstName());
         user.setLastName(registrationBody.getLastName());
         user.setUsername(registrationBody.getUsername());
-        //TODO: Encrypt passwords!!
-        user.setPassword(registrationBody.getPassword());
+        user.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
         user=localUserDAO.save(user);
         return localUserDAO.save(user);
+    }
+
+    public String loginUser(LoginBody loginBody) {
+        Optional<LocalUser> opUser = localUserDAO.findByUsernameIgnoreCase(loginBody.getUsername());
+        if (opUser.isPresent()) {
+            LocalUser user = opUser.get();
+            if (encryptionService.verifyPassword(loginBody.getPassword(), user.getPassword())) {
+                return jwtService.generateJWT(user);
+            }
+        }
+        return null;
     }
 }
